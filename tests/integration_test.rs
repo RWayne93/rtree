f96e9012f8d50a88c2847cb5fd9207deaafaeebb
Ryan <ryantheterror@gmail.com>
2023-10-17 19:52:03 -0400

    i think this second test should fail.

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,22 +1,6 @@
 // use std::process::Command;
 // use std::fs;
 
-// #[test]
-// fn test_print_tree() {
-//     let output = Command::new("cargo")
-//         .arg("run")
-//         .arg("--")
-//         .arg("-d")
-//         .arg("./rtree")  // use rtree as the test directory
-//         .output()
-//         .expect("Failed to execute command");
-
-//     let expected_output = fs::read_to_string("./tests/expected_output.txt")  // replace with a real file
-//         .expect("Failed to read expected output");
-
-//     assert_eq!(String::from_utf8(output.stdout).unwrap(), expected_output);
-// }
-
 use std::process::Command;
 use std::fs;
 
@@ -45,4 +29,20 @@ fn test_print_tree() {
     expected_output_lines.sort();
 
     assert_eq!(output_lines, expected_output_lines);
+}
+
+#[test]
+fn failed_test_print_tree() {
+    let output = Command::new("cargo")
+        .arg("run")
+        .arg("--")
+        .arg("-d")
+        .arg("./rtree")  // use rtree as the test directory
+        .output()
+        .expect("Failed to execute command");
+
+    let expected_output = fs::read_to_string("./tests/expected_output.txt")  // replace with a real file
+        .expect("Failed to read expected output");
+
+    assert_eq!(String::from_utf8(output.stdout).unwrap(), expected_output);
 }
